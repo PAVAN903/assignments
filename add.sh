524ed6cf674ef55cbff875f4dcbebe69cc8e0e0c
PAVAN903 <navap903@gmail.com>
2022-11-11 13:44:24 +0530

    mondaytosunday

--- add.sh
+++ add.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
     
-    toss=$((RANDOM%2))
-            
-      if(($toss == 1))
-          then
-            echo "Heads";
-          else 
-             echo "Tails";
-          fi
+    read -p "enter the number between 1 & 7" day
+     case $day in 
+     1)echo "monday";;
+     2)echo "tuesday";;
+     3)echo "wednesday";;
+     4)echo "thursday";;
+     5)echo "fridayday";;
+     6)echo "saturday";;    
+     7)echo "sunday";
+     esac   
