2de8f8f81a7675da4fb3def1815805165d982c66
PAVAN903 <navap903@gmail.com>
2022-11-11 13:21:54 +0530

    ifloop

--- add.sh
+++ add.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
- read -p "Enter Date:" date
- read -p "Enter Month:" month
-
- if (( ($month <=6 & $date <=20) & ($month >=3 % $date >=20) ))
-  then
-     if(( ($month == 4 & $date ==31) | ($month == 6 & $date ==31) ))
-           then
-            echo $date $month "false";
-         else
-	     echo $date $month "true";
-         fi
-
- elif(( ($month >=3 & $month <6) & ($date >=20 & $date <=31) ))
- then 
-    if(( ($month ==4 & $date ==31) | ($month ==6 & $date ==31) ))
-           then
-            echo $date $month "false";
-          else
-            echo $date $month "true";
-          fi
-
-elif (( ($month >4 & $date <6) & ($date <=31) ))
-  then
-        if(( ($month ==4 & $date ==31) | ($month ==6 & $date ==31) ))
-           then
-            echo $date $month "false";
-        else 
-            echo $date $month "true";
- fi
-        else
-            echo "false";
-        fi
+    
+    toss=$((RANDOM%2))
             
-         
+      if(($toss == 1))
+          then
+            echo "Heads";
+          else 
+             echo "Tails";
+          fi
